b9f04067e395f8488f86040a4e252c9833099f7f
YunYeoMin <iloveyym@gmail.com>
2013-01-24 23:07:05 +0900

    1-1
    1-1

---
+++ src/query/1-1.sql
@@ -0,0 +1,15 @@
+-- Oracle User 持失
+CREATE USER toby
+IDENTIFIED BY r4e3w2q1;
+
+GRANT create session TO toby; 
+
+GRANT create table TO toby;
+
+
+-- Table 持失
+CREATE TABLE users (
+	id VARCHAR2(10) PRIMARY KEY,
+  name VARCHAR2(20) NOT NULL,
+  password VARCHAR2(10) NOT NULL
+);
