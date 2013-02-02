4a6afc7564314fa1936c1b18f34dd236ec518925
YunYeoMin <iloveyym@gmail.com>
2013-02-02 13:28:21 +0900

    1-16

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -7,7 +7,7 @@ public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		UserDao dao = new DaoFactory().userDao();
+		AccountDao dao = new DaoFactory().accountDao();
 
 		User user = new User();
 		user.setId("whiteship");
